d0759d9ad82b20d9c932a0980ec13add629bca35
caroscaj <caroscaj@mail.uc.edu>
2022-10-03 17:33:54 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args)
+    {
+       int age = 22;
+
+       if (age >= 21)
+       {
+           System.out.println("You get a wrist band");
+       }
+       else
+       {
+           System.out.println("You get no wrsit band");
+       }
+
+    }
+}
